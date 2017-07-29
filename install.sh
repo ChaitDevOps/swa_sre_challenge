ae7e011a71b11ac1e8b403b099618bcc07b0b067
swatitangudu <30481735+swatitangudu@users.noreply.github.com>
2017-07-28 20:27:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,3 +17,33 @@ echo 'Ansible playbooks downloaded' >> install.log
 
 # Running Ansible playbook to install Apache and configure
 /opt/ansible/bin/ansible-playbook apache_install_playbook.yml -i "localhost," -e "OS_VERSION=${os_version} CDH_VERSION=${cdh_version} service_acct_usr=${service_acct_usr}" --connection local  >> install.log
+
+cur_hostname=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'`
+echo "Hostname: $cur_hostname" >> install.log
+
+echo "  " >> /etc/httpd/conf/httpd.conf
+echo "LoadModule ssl_module modules/mod_ssl.so" >> /etc/httpd/conf/httpd.conf
+echo "  " >> /etc/httpd/conf/httpd.conf
+echo "Listen 443" >> /etc/httpd/conf/httpd.conf
+
+echo "  " >> /etc/httpd/conf/httpd.conf
+echo "<VirtualHost *:80>" >> /etc/httpd/conf/httpd.conf
+echo "Redirect permanent / https://$cur_hostname:443" >> /etc/httpd/conf/httpd.conf
+echo "</VirtualHost>" >> /etc/httpd/conf/httpd.conf
+echo "  " >> /etc/httpd/conf/httpd.conf
+
+echo "  " >> /etc/httpd/conf/httpd.conf
+echo "<VirtualHost *:443>" >> /etc/httpd/conf/httpd.conf
+echo "ServerName $cur_hostname" >> /etc/httpd/conf/httpd.conf
+echo "SSLEngine on" >> /etc/httpd/conf/httpd.conf
+echo "SSLCertificateFile /etc/httpd/ssl/myserver.crt" >> /etc/httpd/conf/httpd.conf
+echo "SSLCertificateKeyFile /etc/httpd/ssl/myserver.key" >> /etc/httpd/conf/httpd.conf
+echo "</VirtualHost>" >> /etc/httpd/conf/httpd.conf
+echo "  " >> /etc/httpd/conf/httpd.conf
+
+# service httpd restart
+
+# Apache restart and automated test run
+/opt/ansible/bin/ansible-playbook apache_restart.yml -i "localhost," -e "OS_VERSION=${os_version} CDH_VERSION=${cdh_version} service_acct_usr=${service_acct_usr}" --connection local  >> install.log
+
+echo "Apache restarted" >> install.log
